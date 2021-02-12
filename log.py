57502a6c15e0be37c6c362a7f7136b94fd25c8ec
Parul Goyal <pparul157@gmail.com>
2021-02-12 20:08:48 +0530

    Update log.py

--- log.py
+++ log.py
@@ -3,12 +3,12 @@ import os
 start = time.time()
 end = time.time()
 try:
-duration = float(os.environ\[“DURATION”])
+  duration = float(os.environ\[“DURATION”])
 except KeyError:
-duration = 600
+  duration = 600
 i = 1
 while end - start < duration:
-print(f"Log {i}")
-i += 1
-time.sleep(5)
-end = time.time()
+  print(f"Log {i}")
+  i += 1
+  time.sleep(5)
+  end = time.time()

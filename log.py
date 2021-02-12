02694d09ef0f63b2b3c6c40328ff48634aa0b97e
Parul Goyal <pparul157@gmail.com>
2021-02-12 20:17:13 +0530

    Update log.py

--- log.py
+++ log.py
@@ -3,10 +3,10 @@ import os
 start = time.time()
 end = time.time()
 
-duration = 300
+duration = 20
 i = 1
 while end - start < duration:
   print(f"Log {i}")
   i += 1
-  time.sleep(5)
+  time.sleep(0.5)
   end = time.time()

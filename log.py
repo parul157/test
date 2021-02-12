e24ddadf5e59e80ff0e9864d433ad3c0ca1ae434
Parul Goyal <pparul157@gmail.com>
2021-02-12 20:03:40 +0530

    Create log.py

---
+++ log.py
@@ -0,0 +1,14 @@
+import time
+import os
+start = time.time()
+end = time.time()
+try:
+duration = float(os.environ\[“DURATION”])
+except KeyError:
+duration = 600
+i = 1
+while end - start < duration:
+print(f"Log {i}")
+i += 1
+time.sleep(5)
+end = time.time()

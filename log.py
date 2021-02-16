057ee265391c133899a456a26d9527d26bb15af9
Parul Goyal <pparul157@gmail.com>
2021-02-16 14:13:17 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,5 +1,4 @@
 import time
-import sys
 import os
 import logging
 start = time.time()
@@ -8,10 +7,9 @@ end = time.time()
 log = logging.getLogger('parul')
 log.setLevel(logging.DEBUG)
 
-duration = 300
+duration = 30
 i = 1
 while(i <= duration):
   time.sleep(0.5)
   log.debug("parul")
-  sys.stdout.flush()
   i += 1

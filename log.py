88efaf79e951c861bed4d08fa6a9ae488a809cfb
Parul Goyal <pparul157@gmail.com>
2021-02-16 14:16:06 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,15 +1,16 @@
 import time
 import os
-import logging
-start = time.time()
-end = time.time()
+# import logging
+# start = time.time()
+# end = time.time()
 
-log = logging.getLogger('parul')
-log.setLevel(logging.DEBUG)
+# log = logging.getLogger('parul')
+# log.setLevel(logging.DEBUG)
 
 duration = 30
 i = 1
 while(i <= duration):
   time.sleep(0.5)
-  log.debug("parul")
+#   log.debug("parul")
+  print(i)
   i += 1
